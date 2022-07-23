331ec2e3f90e8212f8b01eacd4b331a7db12b92f
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-07-23 11:09:35 +0200

    navbar-ok

---
+++ models/Product.model.js
@@ -0,0 +1,110 @@
+const { Schema, model } = require("mongoose");
+
+// User model Below
+const productSchema = new Schema(
+  {
+    id:{
+      type: Number
+    },
+    productName: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    productDescription: {
+      type: String,
+    },
+    productCost: {
+        type: Number,
+      },
+    productPrice: {
+      exVat: {
+        type: Number,
+        required: true
+      },
+      vat: {
+        type: Number, // we need to adapt VAT with user location
+      },
+      discount: {
+        type: Number, // % de la valeur TTC donc exVat * vat
+      }
+    },
+    materials: [{type: String}],
+    productSize: {
+      sizingTypeLinear: {
+        lenght: {
+          type: Number,
+        },
+        width: {
+          type: Number,
+        },
+        eight: {
+          type: Number,
+        },
+        thickness: {
+          type: Number,
+        },
+        surface: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        }
+      },
+      sizingTypeLetter: {
+        sizeLetter: {
+          type: Number,
+        },
+      },
+      sizingTypeNumber: {
+        sizeNumber: {
+          type: Number,
+        },
+      },
+    },
+    packagingSize: {
+      lenght: {
+        type: Number,
+        required: true
+      },
+      width: {
+        type: Number,
+        required: true
+      },
+      eight: {
+        type: Number,
+        required: true
+      },
+      weight: {
+          type: Number,
+          required: true
+        },
+    },
+    color: {
+      type: String
+    },
+    brand: {
+      type: String
+    },
+    mainPhoto: {
+      type: String, // url
+      required: true
+    },
+    otherPhotos: [{type: String}], // urls
+    stock: {
+        type: Number // has to be edited after each order
+      },
+    category: {
+      type: String
+    },  
+},
+
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,94 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+// User model Below
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+    id:{
+      type: Number
     },
-    password: String,
-  },
+    firstName: {
+      type: String,
+      required : true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type : Number,
+    },
+    email: {
+      type : Number,
+      unique: true,
+      required: true
+    },
+    company: {
+      companyName: {
+        type: String,
+        required: true
+      },
+      vatNumber: {
+        type: String,
+        required: true
+      },
+      apeCode: {
+        type: String
+      },
+      registrationNumber: {
+        type: String,
+        required: true
+      }
+    }, 
+    billingAdress: {
+      streetName: {
+        type: String,
+        required: true
+      },
+      streetNumber: {
+        type: Number,
+        required: true
+      },
+      zipCode: {
+        type: Number,
+        required: true
+      },
+      cityName: {
+        type: String,
+        required: true
+      },
+      countryName: {
+        type: String,
+        required: true
+      }
+    },
+    deliveryAdress: {
+      streetName: {
+        type: String,
+        required: true
+      },
+      streetNumber: {
+        type: Number,
+        required: true
+      },
+      zipCode: {
+        type: Number,
+        required: true
+      },
+      cityName: {
+        type: String,
+        required: true
+      },
+      countryName: {
+        type: String,
+        required: true
+      }
+    },
+    password: { // come back to password because bcrypt / hasch
+      type : String,
+      unique: true,
+      required: true
+    },
+  }, 
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
