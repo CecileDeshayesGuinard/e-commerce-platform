211516d9d049d6ec3c537c8460aa191a7a57fb7c
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-07-24 01:15:18 +0200

    menu

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,29 @@ document.addEventListener(
   },
   false
 );
+
+
+// Gestion du menu déroulant
+const allInvisibleMenu = Array.from(document.querySelectorAll('.wrap-menu'));
+
+const dropDownMenu = document.querySelector('#wrapped-menu');
+const invisibleMenu = document.querySelector('.wrap-menu');
+
+
+allInvisibleMenu.forEach(invisibleMenu => {
+  dropDownMenu.onclick = function () {
+  invisibleMenu.classList.remove('wrap-menu')
+  }
+})
+
+/*
+window.onclick = function (e) {
+  if (!e.target.matches(dropDownMenu)) {
+    invisibleMenu.classList.add('wrap-menu')
+  }
+}
+*/
+
+
+
+
