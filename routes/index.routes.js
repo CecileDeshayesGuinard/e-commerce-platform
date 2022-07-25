2dcb64f69947d837f18ebc434acaa6babdbe1033
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-07-25 18:54:29 +0200

    routes-front ok

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,36 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+/* GET signup page */
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+/* GET account page */
+router.get("/account", (req, res, next) => {
+  res.render("account");
+});
+
+/* GET categories page */
+router.get("/categories", (req, res, next) => {
+  res.render("categories");
+});
+
+/* GET product page */
+router.get("/product", (req, res, next) => {
+  res.render("product");
+});
+
+/* GET cart page */
+router.get("/cart", (req, res, next) => {
+  res.render("cart");
+});
+
+/* GET orders page (for payments) */
+router.get("/orders", (req, res, next) => {
+  res.render("checkout");
+});
+
+// FAIRE DES ROUTES ET TRAVAILLER "not-found" ET "error"
+
 module.exports = router;
