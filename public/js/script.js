2becd12cb7d1fbf958ecc9612b315437e52ca816
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-08-11 01:58:04 +0200

    css-header

--- public/js/script.js
+++ public/js/script.js
@@ -27,12 +27,14 @@ const dropDownCategory = document.querySelector('#category-menu');
 ╩ ╩╩ ╩╩╝╚╝  ╚═╝╩ ╩ ╩ ╚═╝╚═╝╚═╝╩╚═╩╚═╝╚═╝
 */
 
-
 dropDownMenu.onclick = function () {
   hiddenMenu.forEach(function(div) {
     if (div.matches('.wrapped-menu')) {
       div.classList.replace('wrapped-menu', 'unwrapped-menu')
-      console.log('menu unwrapped')
+      if (popup.matches('.upPopup')) {
+        popup.classList.replace('upPopup', 'downPopup');
+      }
+      console.log('menu unwrapped and login popup hidden')
     }
     else {
       if (div.matches('.unwrapped-menu')) {
@@ -71,6 +73,26 @@ dropDownCategory.onclick = function () {
 }
 
 
+/*
+╔═╗╔═╗╔═╗╦ ╦╔═╗  ╦  ╔═╗╔═╗╦╔╗╔
+╠═╝║ ║╠═╝║ ║╠═╝  ║  ║ ║║ ╦║║║║
+╩  ╚═╝╩  ╚═╝╩    ╩═╝╚═╝╚═╝╩╝╚╝
+*/
+
+const loginBtn = document.querySelector('.loginButton');
+const popup = document.querySelector('#popup');
+const unPopup = document.querySelector('.cross');
+const menuIcon = document.querySelector('.menuIcon')
+
+loginBtn.onclick = function () {
+  popup.classList.replace('downPopup', 'upPopup');
+  console.log('login section visible')
+}
+
+unPopup.onclick = function () {
+  popup.classList.replace('upPopup', 'downPopup')
+  console.log('login section hidden')
+}
 
 
 
